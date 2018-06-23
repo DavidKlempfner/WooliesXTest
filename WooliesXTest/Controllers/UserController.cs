ad2c1f673763a21e91c4d44151e3242b8be05337
AVANADE-CORP\david.klempfner <dklempfner@gmail.com>
2018-06-23 23:20:16 +1000

    Exercise1

---
+++ WooliesXTest/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WooliesXTest.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Home
+        public JsonResult Index()
+        {
+            var x = new { name = "test", token = "1234-455662-22233333-3333" };
+            return Json(x, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
